1a6223d2f2d0d38d80199159092ced2450571fc5
Abyab kumar <147365036+Abyab143@users.noreply.github.com>
2024-02-29 23:54:49 +0530

    Update Hack.js

--- Router/Hack.js
+++ Router/Hack.js
@@ -8,7 +8,7 @@ router.post("/hack/create",createHack);
 router.delete("/hack/delete/:id",deleteHack);
 router.put("/hack/update/:id",updateHack);
 router.get("/hack/allHack",getAllHack);
-router.get("/hack/getHack",getHack);
+router.get("/hack/getHack/:id",getHack);
 router.get("/hack/homeHack",getHomeHack);
 
 export default router;
