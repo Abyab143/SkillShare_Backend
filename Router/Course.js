b70bc6d220d016ef637e98e271f85af4b4a507f2
Abyab kumar <147365036+Abyab143@users.noreply.github.com>
2024-02-29 22:45:17 +0530

    Update Course.js

--- Router/Course.js
+++ Router/Course.js
@@ -6,8 +6,8 @@ const router = express.Router();
 //Create a new Course
 router.post("/course/create",createCourse);
 router.get("/course/allcourse",getAllcourse);
-router.delete("/course/delete",deleteCourse);
-router.put("/course/update",updateCourse);
+router.delete("/course/delete/:id",deleteCourse);
+router.put("/course/update/:id",updateCourse);
 router.get("/course/homeCourse",getHomeCourse);
 
 export default router;
